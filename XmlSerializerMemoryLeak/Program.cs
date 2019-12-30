cba891d4c9442c0554dc3b2183712aa46a63c7cf
Paolo <paolovigna@gmail.com>
2019-12-30 18:13:08 +0100

    Moved to github

---
+++ XmlSerializerMemoryLeak/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XmlSerializerMemoryLeak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var s = new XmlSerializer(typeof(object), new Type[] { });
+                s.Serialize(Stream.Null, new object { });
+            }
+        }
+    }
+}
---
+++ XmlSerializerNoMemoryLeak/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlSerializerNoMemoryLeak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = Guid.NewGuid();
+            while (true)
+            {
+                var s = XmlSerializerCache.Create(typeof(object), key.ToString());
+                s.Serialize(Stream.Null, new object { });
+            }
+        }
+    }
+
+    public static class XmlSerializerCache
+    {
+        private static readonly Dictionary<string, System.Xml.Serialization.XmlSerializer> Cache =
+            new Dictionary<string, System.Xml.Serialization.XmlSerializer>();
+
+        public static System.Xml.Serialization.XmlSerializer Create(Type type, string key)
+        {
+
+            if (!Cache.ContainsKey(key))
+            {
+                Cache.Add(key, new System.Xml.Serialization.XmlSerializer(type, new Type[] { }));
+            }
+
+            return Cache[key];
+        }
+    }
+}
